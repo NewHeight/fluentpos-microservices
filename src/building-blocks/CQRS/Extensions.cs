1a3fb7a9716c9777251fad1a7024821ba51fa015
Mukesh Murugan <iammukeshm@gmail.com>
2022-11-05 18:08:44 +0530

    mediatr

---
+++ src/building-blocks/CQRS/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Logging;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.CQRS
+{
+    public static class Extensions
+    {
+        public static IServiceCollection UseCommonMediatR(this IServiceCollection services, Assembly mediatrAssembly, bool enableLoggingBehavior = true)
+        {
+            services.AddMediatR(mediatrAssembly);
+            if (enableLoggingBehavior) services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            return services;
+        }
+    }
+}
