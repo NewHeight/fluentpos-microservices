b0cf08c359f3ef0c9f6ad6ae8889a7c3a9a06bbd
Mukesh Murugan <iammukeshm@gmail.com>
2022-11-06 15:37:28 +0530

    added static files serving in gateway

--- src/api-gateway/Program.cs
+++ src/api-gateway/Program.cs
@@ -3,5 +3,8 @@ builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSecti
 
 var app = builder.Build();
 app.MapGet("/", () => "Hello From Gateway");
+app.UseStaticFiles(); // The middleware runs before routing happens => no route was found
+
+app.UseRouting();
 app.MapReverseProxy();
 app.Run();
